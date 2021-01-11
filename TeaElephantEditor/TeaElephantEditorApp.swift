c869942d1aa86ad36ef7eed49bc07824ad26e3ed
lueurxax <lueurxax@gmail.com>
2021-01-12 00:31:35 +0300

    Initial Commit

---
+++ TeaElephantEditor/TeaElephantEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeaElephantEditorApp.swift
+//  TeaElephantEditor
+//
+//  Created by Andrew Khasanov on 12.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct TeaElephantEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
